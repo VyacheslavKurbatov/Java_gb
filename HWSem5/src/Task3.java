46fbfe3fb0df4cf3e65244c86025619ffc0a57a4
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2023-02-26 21:49:09 +0300

    Add HWSem5 heapSort

--- HWSem5/src/Task3.java
+++ HWSem5/src/Task3.java
@@ -1,4 +1,56 @@
+import java.util.Arrays;
+import java.util.Random;
+
 //Реализовать алгоритм пирамидальной сортировки (HeapSort)
 public class Task3 {
-   //TODO
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Random random = new Random();
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = random.nextInt(0, 10);
+            arr[i] = num;
+        }
+        System.out.println(Arrays.toString(arr));
+
+        heapSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+    public static void heapify(int[] array, int length, int i) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+        if (left < length && array[left] > array[largest]) {
+            largest = left;
+        }
+        if (right < length && array[right] > array[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            int tmp = array[i];
+            array[i] = array[largest];
+            array[largest] = tmp;
+            heapify(array, length, largest);
+        }
+    }
+    public static void heapSort(int[] array) {
+        if (array.length == 0) {
+            return;
+        }
+
+        int length = array.length;
+
+        for (int i = length / 2 - 1; i >= 0; i--) {
+            heapify(array, length, i);
+        }
+
+        for (int i = length - 1; i >= 0; i--) {
+            int tmp = array[0];
+            array[0] = array[i];
+            array[i] = tmp;
+            heapify(array, i, 0);
+        }
+    }
 }
